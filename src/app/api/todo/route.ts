9a2843ca05ef32150161d250e8299906adc16aa5
Yuto Urashima <yuto.urashima24@gmail.com>
2024-03-24 23:22:26 +0900

    feat: todoを登録するapiを作成

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -2,6 +2,18 @@ import prisma from '@/lib/prisma/client'
 import { NextResponse } from 'next/server'
 
 export async function GET(req: Request) {
-  const allTodos = await prisma.todo.findMany()
-  return NextResponse.json(allTodos)
+  const allTodo = await prisma.todo.findMany()
+  return NextResponse.json(allTodo)
+}
+
+export async function POST(req: Request) {
+  const { title, userId } = await req.json()
+
+  const allTodo = await prisma.todo.create({
+    data: {
+      title,
+      userId
+    }
+  })
+  return NextResponse.json(allTodo)
 }
