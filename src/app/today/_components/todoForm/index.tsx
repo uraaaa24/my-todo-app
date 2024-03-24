293d00a9b8767933f7ddac80307c64d7dfbd8f51
Yuto Urashima <yuto.urashima24@gmail.com>
2024-03-24 23:15:19 +0900

    style: ファイル名やコメントを追加

--- src/app/today/_components/todoForm/index.tsx
+++ src/app/today/_components/todoForm/index.tsx
@@ -7,22 +7,29 @@ import { z } from 'zod'
 import TitleInput from './titleInput'
 
 const formSchema = z.object({
-  title: z.string().max(100, { message: '100文字以内で入力してください' })
+  title: z.string().max(50, { message: '50文字以内で入力してください' })
 })
 
+/**
+ * ToDoのフォームコンポーネント
+ */
 const ToDoForm = () => {
   const methods = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: { title: '' }
   })
 
-  const onSubmit = async () => {}
+  const onSubmit = async () => {
+    console.log('submit')
+  }
 
   return (
     <FormProvider {...methods}>
       <Box component="form" onSubmit={methods.handleSubmit(onSubmit)} sx={{ display: 'flex', gap: 2, mb: 2 }}>
         <TitleInput />
-        <Button variant="contained">Add</Button>
+        <Button type="submit" variant="contained">
+          Add
+        </Button>
       </Box>
     </FormProvider>
   )
--- src/app/today/_components/todoForm/titleInput/index.tsx
+++ src/app/today/_components/todoForm/titleInput/index.tsx
@@ -1,10 +1,26 @@
-import { Input } from '@mui/material'
+import { FormHelperText, Input } from '@mui/material'
 import { Controller, useFormContext } from 'react-hook-form'
 
+/**
+ * ToDoのタイトルを入力するコンポーネント
+ */
 const TitleInput = () => {
   const { control } = useFormContext()
 
-  return <Controller control={control} name="title" render={({ field }) => <Input {...field} required />} />
+  return (
+    <>
+      <Controller
+        control={control}
+        name="title"
+        render={({ field, fieldState }) => (
+          <>
+            <Input fullWidth {...field} />
+            <FormHelperText error>{fieldState.error?.message}</FormHelperText>
+          </>
+        )}
+      />
+    </>
+  )
 }
 
 export default TitleInput
