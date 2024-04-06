62a3f593c7b0565f341c1504c89bb5663a4e3fb6
Yuto Urashima <yuto.urashima24@gmail.com>
2024-04-06 21:00:14 +0900

    fix: todoを未入力で登録できないようにバリデーションを修正

--- src/schemas/todoTitleInput/validation.ts
+++ src/schemas/todoTitleInput/validation.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import { TodoFormNames } from '.'
 
+const MIN_TITLE_LENGTH = 1
 const MAX_TITLE_LENGTH = 50
 
 /** Todoを入力するフォームのスキーマ */
@@ -13,10 +14,16 @@ export type TodoFormSchemaInferType = z.infer<TodoFormObjectSchema>
 
 /** Todoを入力するフォームで使用するバリデーションメッセージ */
 const TodoFormValidationMessages = {
-  [TodoFormNames.title]: '50文字以内で入力してください'
+  [TodoFormNames.title]: {
+    min: 'タイトルを入力してください',
+    max: `${MAX_TITLE_LENGTH}文字以内で入力してください`
+  }
 }
 
 /** Todoを入力するフォームのバリデーションスキーマ */
 export const TodoFormValidationSchema: TodoFormObjectSchema = z.object({
-  [TodoFormNames.title]: z.string().max(MAX_TITLE_LENGTH, { message: TodoFormValidationMessages[TodoFormNames.title] })
+  [TodoFormNames.title]: z
+    .string()
+    .min(MIN_TITLE_LENGTH, { message: TodoFormValidationMessages[TodoFormNames.title].min })
+    .max(MAX_TITLE_LENGTH, { message: TodoFormValidationMessages[TodoFormNames.title].min })
 })
